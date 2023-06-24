cfd3def02b5c998d833e2d8fe9d8554489d9b514
Nick Bicknell <nicholasbicknell@gmail.com>
2023-06-23 19:12:09 -0500

    added containers and designed the layout of html, added css styling to elements, created some js variables

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+var homeEl = document.querySelector("#home");
+var startBtnEl = document.querySelector("#start-btn");
+var quizEl = document.querySelector("#quiz");
+var questionEl = document.querySelector("#question");
+var answerEl = document.querySelector("#quiz");
+var timerEl = document.querySelector("#timer");
+
+
+
